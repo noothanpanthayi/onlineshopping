cffcb29659f17d32f0a981c66a73b71602964bca
noothanpanthayi <noothankrishnan@outlook.com>
2024-11-04 21:35:37 -0600

    update

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -23,7 +23,7 @@ const Page = async () => {
         This NextJs AppRouter application makes an HTTP request to a free API url, renders the products list at the server-side and then sends to the client.
         Once it reaches the client, it gets hydrated and Add to Cart button becomes active.
         </div>
-        <div className="hdrpanel">Product Listing Page <span className="subtitle">&nbsp;(NextJs Server Side Rendered)</span></div>
+        <div className="hdrpanel">Product Listing Page</div>
       </div>
 
       <div className="vspacer"></div>
