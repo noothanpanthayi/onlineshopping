cef0a7a0f0a093d17b24c8b2f6b212ee1946ad41
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-09 15:35:22 -0600

    update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,38 @@
 module.exports = {
-    images: {
-      domains:[
-     'cdn.dummyjson.com',
-     'fakestoreapi.com',
-     'i.imgur.com'
-      ],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.imgur.com',
-          port: '3000',
-          pathname: '/**',
-        },
-      ],
-    },
-  }
+  images: {
+    domains: [
+      'cdn.dummyjson.com',
+      'fakestoreapi.com',
+      'i.imgur.com'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '3000',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, HEAD, OPTIONS, POST, PUT, PATCH, DELETE'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization'
+          }
+        ],
+      },
+    ]
+  },
+}
